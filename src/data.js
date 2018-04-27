0079092d8177edb84edaa5d1c9db2f6434c9daec
Miguel Coquet <miguel@mcoquet.com>
2018-04-27 16:23:54 +0100

    all the new things

---
+++ src/data.js
@@ -0,0 +1,32 @@
+import PouchDB from 'pouchdb'
+
+const remoteDB = new PouchDB("http://localhost:5984/jobboard");
+
+var myDB = PouchDB('jobboard')
+
+function Job () {
+  return {
+    id: 1,
+    raw: 'hcueihfgeuorhfioerhfier',
+    Poster: "miguelcoquet",
+    links: []
+  }
+}
+
+function Poster () {
+  return {
+    id: "miguelcoquet",
+  }
+}
+
+
+const DB = {
+  PouchDB,
+  name: 'meow',
+  db: myDB, 
+  init: () => {
+    return myDB.sync(remoteDB, {live: true});
+  }
+}
+
+export default DB
